d93b262b93d01a8bc880de22a4fc89cf804d037b
tonymushah <tonyramanantsalama@gmail.com>
2023-08-21 01:20:08 +0300

    CoverUtils refactor

--- src/utils/cover.rs
+++ src/utils/cover.rs
@@ -2,132 +2,135 @@ use std::{fs::File, path::Path};
 
 use async_stream::stream;
 use futures::Stream;
+use mangadex_api::HttpClientRef;
 use mangadex_api_schema_rust::{v5::CoverAttributes, ApiData, ApiObject};
 
 use crate::settings::files_dirs::DirsOptions;
 
-pub fn is_cover_image_there(cover_id: String) -> Result<bool, std::io::Error> {
-    if !cover_id.is_empty() {
-        let path = match DirsOptions::new() {
-            core::result::Result::Ok(data) => data,
-            Err(e) => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    e.to_string(),
-                ))
-            }
-        }
-        .covers_add(format!("{}.json", cover_id).as_str());
-        let cover_data: ApiData<ApiObject<CoverAttributes>> =
-            serde_json::from_reader(File::open(path)?)?;
-        let cover_file_name = cover_data.data.attributes.file_name;
-        let cover_file_name_path = match DirsOptions::new() {
-            core::result::Result::Ok(data) => data,
-            Err(e) => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    e.to_string(),
-                ))
-            }
-        }
-        .covers_add(format!("images/{}", cover_file_name).as_str());
-        if Path::new(cover_file_name_path.as_str()).exists() {
-            std::io::Result::Ok(true)
-        } else {
-            std::io::Result::Ok(false)
-        }
-    } else {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "the cover_id should'nt be empty",
-        ))
-    }
+use super::{chapter::ChapterUtils, manga::MangaUtils};
+
+#[derive(Clone)]
+pub struct CoverUtils {
+    pub(crate) dirs_options: DirsOptions,
+    pub(crate) http_client_ref: HttpClientRef,
 }
 
-pub fn is_cover_there(cover_id: String) -> Result<bool, std::io::Error> {
-    if !cover_id.is_empty() {
-        let path = match DirsOptions::new() {
-            core::result::Result::Ok(data) => data,
-            Err(e) => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    e.to_string(),
-                ))
-            }
+impl CoverUtils {
+    pub fn new(dirs_options: DirsOptions, http_client_ref: HttpClientRef) -> Self {
+        Self {
+            dirs_options,
+            http_client_ref,
         }
-        .covers_add(format!("{}.json", cover_id).as_str());
-        if Path::new(path.as_str()).exists() {
-            is_cover_image_there(cover_id)
-        } else {
-            std::io::Result::Ok(false)
-        }
-    } else {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "the cover_id should'nt be empty",
-        ))
     }
-}
-
-pub fn get_cover_data(
-    cover_id: String,
-) -> Result<ApiData<ApiObject<CoverAttributes>>, std::io::Error> {
-    let cover_id_clone = cover_id.clone();
-    match is_cover_there(cover_id) {
-        core::result::Result::Ok(is_there) => {
-            if is_there {
-                let path = match DirsOptions::new() {
-                    core::result::Result::Ok(data) => data,
-                    Err(e) => {
-                        return Err(std::io::Error::new(
-                            std::io::ErrorKind::Other,
-                            e.to_string(),
-                        ))
-                    }
-                }
-                .covers_add(format!("{}.json", cover_id_clone).as_str());
-                let data: ApiData<ApiObject<CoverAttributes>> =
-                    serde_json::from_str(std::fs::read_to_string(path)?.as_str())?;
-                core::result::Result::Ok(data)
+    pub fn is_cover_there(&self, cover_id: String) -> Result<bool, std::io::Error> {
+        if !cover_id.is_empty() {
+            let path = self
+                .dirs_options
+                .covers_add(format!("{}.json", cover_id).as_str());
+            if Path::new(path.as_str()).exists() {
+                self.is_cover_image_there(cover_id)
             } else {
-                Err(std::io::Error::new(
-                    std::io::ErrorKind::NotFound,
-                    "Cover not found",
-                ))
+                std::io::Result::Ok(false)
             }
-        }
-        Err(error) => Err(error),
-    }
-}
-
-pub fn get_all_cover() -> Result<impl Stream<Item = String>, std::io::Error> {
-    let file_dirs = match DirsOptions::new() {
-        core::result::Result::Ok(data) => data,
-        Err(error) => {
-            return Err(std::io::Error::new(
+        } else {
+            Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
-                error.to_string(),
+                "the cover_id should'nt be empty",
             ))
         }
-    };
-    let path = file_dirs.covers_add("");
-    if Path::new(path.as_str()).exists() {
-        let list_dir = (std::fs::read_dir(path.as_str()))?;
-        Ok(stream! {
-            for file_ in list_dir.flatten() {
-                if let core::result::Result::Ok(metadata) = file_.metadata() {
-                    if metadata.is_file() {
-                        if let Some(data) = file_.file_name().to_str() {
-                            yield data.to_string().replace(".json", "");
+    }
+    pub fn is_cover_image_there(&self, cover_id: String) -> Result<bool, std::io::Error> {
+        if !cover_id.is_empty() {
+            let path = self
+                .dirs_options
+                .covers_add(format!("{}.json", cover_id).as_str());
+            let cover_data: ApiData<ApiObject<CoverAttributes>> =
+                serde_json::from_reader(File::open(path)?)?;
+            let cover_file_name = cover_data.data.attributes.file_name;
+            let cover_file_name_path = self
+                .dirs_options
+                .covers_add(format!("images/{}", cover_file_name).as_str());
+            if Path::new(cover_file_name_path.as_str()).exists() {
+                std::io::Result::Ok(true)
+            } else {
+                std::io::Result::Ok(false)
+            }
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "the cover_id should'nt be empty",
+            ))
+        }
+    }
+    pub fn get_cover_data(
+        &self,
+        cover_id: String,
+    ) -> Result<ApiData<ApiObject<CoverAttributes>>, std::io::Error> {
+        let cover_id_clone = cover_id.clone();
+        match self.is_cover_there(cover_id) {
+            core::result::Result::Ok(is_there) => {
+                if is_there {
+                    let path = self
+                        .dirs_options
+                        .covers_add(format!("{}.json", cover_id_clone).as_str());
+                    let data: ApiData<ApiObject<CoverAttributes>> =
+                        serde_json::from_str(std::fs::read_to_string(path)?.as_str())?;
+                    core::result::Result::Ok(data)
+                } else {
+                    Err(std::io::Error::new(
+                        std::io::ErrorKind::NotFound,
+                        format!("Cover {cover_id} not found"),
+                    ))
+                }
+            }
+            Err(error) => Err(error),
+        }
+    }
+    pub fn get_all_cover<'a>(&'a self) -> Result<impl Stream<Item = String> + 'a, std::io::Error> {
+        let file_dirs = self.dirs_options.clone();
+        let path = file_dirs.covers_add("");
+        if Path::new(path.as_str()).exists() {
+            let list_dir = (std::fs::read_dir(path.as_str()))?;
+            Ok(stream! {
+                for file_ in list_dir.flatten() {
+                    if let core::result::Result::Ok(metadata) = file_.metadata() {
+                        if metadata.is_file() {
+                            if let Some(data) = file_.file_name().to_str() {
+                                yield data.to_string().replace(".json", "");
+                            }
                         }
                     }
                 }
-            }
-        })
-    } else {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            "can't find the cover directory",
-        ))
+            })
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "can't find the cover directory",
+            ))
+        }
+    }
+}
+
+impl From<ChapterUtils> for CoverUtils {
+    fn from(value: ChapterUtils) -> Self {
+        Self::new(value.dirs_options, value.http_client_ref)
+    }
+}
+
+impl<'a> From<&'a ChapterUtils> for CoverUtils {
+    fn from(value: &'a ChapterUtils) -> Self {
+        Self::new(value.dirs_options, value.http_client_ref)
+    }
+}
+
+impl From<MangaUtils> for CoverUtils {
+    fn from(value: MangaUtils) -> Self {
+        Self::new(value.dirs_options, value.http_client_ref)
+    }
+}
+
+impl<'a> From<&'a MangaUtils> for CoverUtils {
+    fn from(value: &'a MangaUtils) -> Self {
+        Self::new(value.dirs_options, value.http_client_ref)
     }
 }
