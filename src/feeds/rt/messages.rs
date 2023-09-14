e4573539938f40cae0db74027736a5b5e89d90b1
tonymushah <tonyramanantsalama@gmail.com>
2023-09-14 18:24:34 +0300

    Removing tauri-feature

--- src/feeds/rt/messages.rs
+++
@@ -1,8 +0,0 @@
-use actix::Message;
-
-use crate::{feeds::MangaDownloadFeedError, utils::feed::ChapterFeed};
-pub type FeedRtResult = Result<ChapterFeed, MangaDownloadFeedError>;
-
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct FeedRtMessage(pub FeedRtResult);
