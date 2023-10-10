74b2d4da24351fe0461756d9ede4e7375720e8b5
tonymushah <tonyramanantsalama@gmail.com>
2023-10-10 06:23:44 +0300

    starHaving doubt

--- src/utils/chapter/get_all_chapter.rs
+++ src/utils/chapter/get_all_chapter.rs
@@ -4,6 +4,7 @@ use crate::settings::file_history::{History, IsIn};
 
 use super::GetAllChapter;
 use futures::StreamExt;
+use log::info;
 use tokio_stream::Stream;
 
 pub struct OnlyFails<T>
@@ -13,16 +14,25 @@ where
     inner: T,
 }
 
-impl<T> OnlyFails<T> where T: Stream<Item = String> + Unpin {
-    pub fn new(inner : T) -> Self{
+impl<T> OnlyFails<T>
+where
+    T: Stream<Item = String> + Unpin,
+{
+    pub fn new(inner: T) -> Self {
         Self { inner }
     }
 }
 
-impl<T> Stream for OnlyFails<T> where T: Stream<Item = String> + Unpin {
+impl<T> Stream for OnlyFails<T>
+where
+    T: Stream<Item = String> + Unpin,
+{
     type Item = String;
 
-    fn poll_next(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Option<Self::Item>> {
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
         self.inner.poll_next_unpin(cx)
     }
 }
@@ -32,43 +42,56 @@ where
     T: Stream<Item = String> + Unpin,
 {
     all_chapter: T,
-    history: History
+    history: Vec<String>,
 }
 
-impl<T> NotIncludeFails<T> where T: Stream<Item = String> + Unpin  {
-    pub fn new(all_chapter: T, history: History) -> Self{
-        Self { all_chapter, history }
+impl IsIn<String> for Vec<String> {
+    type Output = Option<usize>;
+    fn is_in(&self, to_use: String) -> Self::Output {
+        let pos = self.iter().position(|d| d.cmp(&to_use).is_eq());
+        info!("{:?}", pos);
+        pos
     }
 }
 
-impl<T> Stream for NotIncludeFails<T> where T: Stream<Item = String> + Unpin  {
-    type Item = String;
+impl<T> NotIncludeFails<T>
+where
+    T: Stream<Item = String> + Unpin,
+{
+    pub fn new(all_chapter: T, history: History) -> Self {
+        Self {
+            all_chapter,
+            history: history
+                .get_history_list()
+                .iter()
+                .map(|d| d.to_string())
+                .collect(),
+        }
+    }
+}
 
-    fn poll_next(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Option<Self::Item>> {
+impl<T> Stream for NotIncludeFails<T>
+where
+    T: Stream<Item = String> + Unpin,
+{
+    type Item = (String, bool);
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
         if let Poll::Ready(getted) = self.all_chapter.poll_next_unpin(cx) {
             if let Some(id) = getted {
-                if let Ok(uuid) = <uuid::Uuid as TryFrom<&str>>::try_from(id.clone().as_str()){
-                    if self.history.is_in(uuid).is_none() {
-                        log::info!("{uuid}");
-                        Poll::Ready(Some(id))
-                    }else {
-                        log::info!("Pending 3");
-                        Poll::Pending
-                    }
-                }else {
-                    log::info!("Pending 2");
-                    Poll::Pending
-                }
-            }else {
+                Poll::Ready(Some((id, self.history.is_in(id.clone()).is_none())))
+            } else {
                 log::info!("Exited");
                 Poll::Ready(None)
             }
-        }else {
+        } else {
             log::info!("Pending 1");
             Poll::Pending
         }
     }
-    
 }
 
 pub struct AsyncGetAllChapter<C, H>
@@ -78,24 +101,27 @@ where
 {
     pub only_fails: OnlyFails<H>,
     pub parameters: GetAllChapter,
-    pub not_fails: NotIncludeFails<C>
+    pub not_fails: NotIncludeFails<C>,
 }
 
 impl<C, H> Stream for AsyncGetAllChapter<C, H>
 where
     C: Stream<Item = String> + std::marker::Unpin,
-    H: Stream<Item = String> + std::marker::Unpin,  
+    H: Stream<Item = String> + std::marker::Unpin,
 {
     type Item = String;
 
-    fn poll_next(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Option<Self::Item>> {
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
         if self.parameters.only_fails {
             log::info!("Only fails");
             self.only_fails.poll_next_unpin(cx)
-        }else if !self.parameters.include_fails {
+        } else if !self.parameters.include_fails {
             log::info!("not fails");
-            self.not_fails.poll_next_unpin(cx)
-        }else{
+            self.not_fails.filter(|(_, b)| async move { !b }).map(|(id, _)| id).poll_next_unpin(cx)
+        } else {
             log::info!("all chapter");
             self.not_fails.all_chapter.poll_next_unpin(cx)
         }
