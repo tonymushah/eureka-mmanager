aa123bbac6f62022cf5348efcb3f0afe3e06860a
tonymushah <tonyramanantsalama@gmail.com>
2024-05-02 14:05:55 +0300

    Added `InsertMessage`

--- src/history/service/messages.rs
+++ src/history/service/messages.rs
@@ -1,2 +1,3 @@
 pub mod commit;
+pub mod insert;
 pub mod rollback;
---
+++ src/history/service/messages/insert.rs
@@ -0,0 +1,101 @@
+use std::ops::{Deref, DerefMut};
+
+use actix::prelude::*;
+
+use crate::{
+    history::{
+        history_w_file::traits::{AsyncAutoCommitRollbackInsert, AutoCommitRollbackInsert},
+        service::HistoryActorService,
+        AsyncInsert, HistoryEntry, Insert,
+    },
+    ManagerCoreResult,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct InsertMessage {
+    entry: HistoryEntry,
+    auto_commit: bool,
+}
+
+impl InsertMessage {
+    pub fn new(entry: HistoryEntry) -> Self {
+        Self {
+            entry,
+            auto_commit: true,
+        }
+    }
+    pub fn no_commit(self) -> Self {
+        Self {
+            entry: self.entry,
+            auto_commit: false,
+        }
+    }
+    pub fn commit(self) -> Self {
+        Self {
+            entry: self.entry,
+            auto_commit: true,
+        }
+    }
+}
+
+impl Deref for InsertMessage {
+    type Target = HistoryEntry;
+    fn deref(&self) -> &Self::Target {
+        &self.entry
+    }
+}
+
+impl DerefMut for InsertMessage {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.entry
+    }
+}
+
+impl From<InsertMessage> for HistoryEntry {
+    fn from(value: InsertMessage) -> Self {
+        value.entry
+    }
+}
+
+impl From<HistoryEntry> for InsertMessage {
+    fn from(value: HistoryEntry) -> Self {
+        InsertMessage::new(value)
+    }
+}
+
+impl Message for InsertMessage {
+    type Result = ManagerCoreResult<()>;
+}
+
+impl Handler<InsertMessage> for HistoryActorService {
+    type Result = ManagerCoreResult<()>;
+    fn handle(&mut self, msg: InsertMessage, _ctx: &mut Self::Context) -> Self::Result {
+        if msg.auto_commit {
+            <Self as AutoCommitRollbackInsert<HistoryEntry>>::insert(self, msg.entry)
+        } else {
+            <Self as Insert<HistoryEntry>>::insert(self, msg.entry)
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl<'a> AsyncAutoCommitRollbackInsert<'a, HistoryEntry> for Addr<HistoryActorService> {
+    type Output = ManagerCoreResult<()>;
+    async fn insert(
+        &'a mut self,
+        value: HistoryEntry,
+    ) -> <Self as AsyncAutoCommitRollbackInsert<HistoryEntry>>::Output {
+        self.send(InsertMessage::new(value)).await?
+    }
+}
+
+#[async_trait::async_trait]
+impl<'a> AsyncInsert<'a, HistoryEntry> for Addr<HistoryActorService> {
+    type Output = ManagerCoreResult<()>;
+    async fn insert(
+        &'a mut self,
+        value: HistoryEntry,
+    ) -> <Self as AsyncInsert<HistoryEntry>>::Output {
+        self.send(InsertMessage::new(value).no_commit()).await?
+    }
+}
