484e3938fb1891ba4a8342ae2e7c424bc5fea156
tonymushah <tonyramanantsalama@gmail.com>
2024-05-03 10:55:13 +0300

    Added manga_list bin

---
+++ src/bin/manga_lists.rs
@@ -0,0 +1,31 @@
+use actix::prelude::*;
+use mangadex_desktop_api2::{
+    files_dirs::messages::manga_list_data_pull::MangaListDataPullMessage,
+    history::{
+        service::{messages::is_in::IsInMessage, HistoryActorService},
+        HistoryEntry,
+    },
+    DirsOptions,
+};
+use tokio_stream::StreamExt;
+
+fn main() -> anyhow::Result<()> {
+    let run = System::new();
+    run.block_on(async {
+        let options = DirsOptions::new_from_data_dir("data");
+        options.verify_and_init()?;
+        let options_actor = options.start();
+        let history = HistoryActorService::new(options_actor.clone())
+            .await
+            .start();
+        let mut data_pull = options_actor.send(MangaListDataPullMessage).await??;
+        while let Some(manga) = data_pull.next().await {
+            let has_failed = history
+                .send(IsInMessage(HistoryEntry::new(manga.id, manga.type_)))
+                .await?;
+            println!("{:#?} - {has_failed}", manga.id);
+        }
+        Ok::<(), anyhow::Error>(())
+    })?;
+    Ok(())
+}
