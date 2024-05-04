c4f5e8bb5343c0d1ca5a1983fbb89e0db1af37e3
tonymushah <tonyramanantsalama@gmail.com>
2024-05-04 11:56:54 +0300

    Added exports in `manga` data_pull

--- src/data_pulls/manga.rs
+++ src/data_pulls/manga.rs
@@ -1,3 +1,9 @@
 pub mod filter;
 pub mod ids;
 pub mod list;
+
+pub use filter::{
+    IntoMangaListDataPullFilter, MangaListDataPullFilter, MangaListDataPullFilterParams,
+};
+pub use ids::MangaIdsListDataPull;
+pub use list::MangaListDataPull;
