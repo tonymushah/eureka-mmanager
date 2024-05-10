0b72636137475876b2deb993689091fbdcf12ef0
tonymushah <tonyramanantsalama@gmail.com>
2024-05-10 21:17:22 +0300

    implemented `verify_and_push` for `impl Push<MangaObject> for DirsOptions`

--- src/data_push.rs
+++ src/data_push.rs
@@ -2,6 +2,10 @@ pub mod chapter;
 pub mod cover;
 pub mod manga;
 
+use itertools::Itertools;
+use mangadex_api_schema_rust::ApiObject;
+use mangadex_api_types_rust::RelationshipType;
+
 use crate::ManagerCoreResult;
 
 pub trait Push<T> {
@@ -9,4 +13,16 @@ pub trait Push<T> {
     fn verify_and_push(&mut self, data: T) -> ManagerCoreResult<()> {
         self.push(data)
     }
+}
+
+pub(crate) fn seed_rel<A>(input: &mut ApiObject<A>, seed: &ApiObject<A>, rel: RelationshipType) {
+    input.relationships.retain(|x| x.type_ != rel);
+    input.relationships.append(
+        &mut seed
+            .relationships
+            .iter()
+            .filter(|r| r.type_ == rel)
+            .cloned()
+            .collect_vec(),
+    );
 }
