a99d5c0ff1a4f52342251a3f6e95fbfc67554865
tonymushah <tonyramanantsalama@gmail.com>
2024-05-10 22:23:43 +0300

    implemented `impl<T> Handler<PushDataMessage<T>> for DirsOptions`

--- src/files_dirs/messages/push.rs
+++ src/files_dirs/messages/push.rs
@@ -1,6 +1,6 @@
 use actix::prelude::*;
 
-use crate::ManagerCoreResult;
+use crate::{data_push::Push, DirsOptions, ManagerCoreResult};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct PushDataMessage<T> {
@@ -18,5 +18,19 @@ impl<T> PushDataMessage<T> {
     }
     pub fn verify(self, verify: bool) -> Self {
         Self { verify, ..self }
+    }
+}
+
+impl<T> Handler<PushDataMessage<T>> for DirsOptions
+where
+    Self: Push<T>,
+{
+    type Result = <PushDataMessage<T> as Message>::Result;
+    fn handle(&mut self, msg: PushDataMessage<T>, _ctx: &mut Self::Context) -> Self::Result {
+        if msg.verify {
+            self.verify_and_push(msg.data)
+        } else {
+            self.push(msg.data)
+        }
     }
 }
