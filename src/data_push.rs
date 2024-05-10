11e562beb49ab168a3ac2fee166011b3a0849e76
tonymushah <tonyramanantsalama@gmail.com>
2024-05-10 17:52:46 +0300

    added `DataPush`

---
+++ src/data_push.rs
@@ -0,0 +1,23 @@
+use std::ops::{Deref, DerefMut};
+
+use crate::{DirsOptions, ManagerCoreResult};
+
+#[derive(Debug)]
+pub struct DataPush<'a>(&'a mut DirsOptions);
+
+impl<'a> Deref for DataPush<'a> {
+    type Target = DirsOptions;
+    fn deref(&self) -> &Self::Target {
+        self.0
+    }
+}
+
+impl<'a> DerefMut for DataPush<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.0
+    }
+}
+
+pub trait Push<T> {
+    fn push(&mut self, data: &T) -> ManagerCoreResult<()>;
+}
