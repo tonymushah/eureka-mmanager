90f5f3f00b2cc33ba864c770dee46554a3326efa
tonymushah <tonyramanantsalama@gmail.com>
2024-05-27 14:54:11 +0300

    implemented `GetManagerStateMessage` and `SubscribeToManagerMessage` for `ChapterDownloadManager`

---
+++ src/download/chapter/messages.rs
@@ -0,0 +1,19 @@
+use actix::{Handler, Message};
+
+use crate::download::messages::{state::GetManagerStateMessage, SubcribeToManagerMessage};
+
+use super::ChapterDownloadManager as Manager;
+
+impl Handler<GetManagerStateMessage> for Manager {
+    type Result = <GetManagerStateMessage as Message>::Result;
+    fn handle(&mut self, _msg: GetManagerStateMessage, _ctx: &mut Self::Context) -> Self::Result {
+        self.state.clone()
+    }
+}
+
+impl Handler<SubcribeToManagerMessage> for Manager {
+    type Result = <SubcribeToManagerMessage as Message>::Result;
+    fn handle(&mut self, _msg: SubcribeToManagerMessage, _ctx: &mut Self::Context) -> Self::Result {
+        self.notify.clone()
+    }
+}
