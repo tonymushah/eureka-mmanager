c1fa1023aa1b8c1afa953662e5e179e7009ab741
tonymushah <tonyramanantsalama@gmail.com>
2024-06-11 09:29:24 +0300

    implemented `State` for `CoverDownloadTask`

--- src/download/cover/task/messages/state.rs
+++ src/download/cover/task/messages/state.rs
@@ -3,12 +3,22 @@ use std::ops::Deref;
 use actix::prelude::*;
 
 use crate::download::{
-    cover::task::CoverDownloadTask as Task, messages::TaskStateMessage, state::TaskState,
+    cover::task::{CoverDownloadTask as Task, CoverDownloadTaskState},
+    messages::TaskStateMessage,
+    state::TaskState,
+    traits::State,
 };
 
 impl Handler<TaskStateMessage> for Task {
     type Result = TaskState;
     fn handle(&mut self, _msg: TaskStateMessage, _ctx: &mut Self::Context) -> Self::Result {
-        self.sender.borrow().deref().into()
+        self.state()
+    }
+}
+
+impl State for Task {
+    type State = CoverDownloadTaskState;
+    fn inner_state(&self) -> Self::State {
+        self.sender.borrow().deref().clone()
     }
 }
