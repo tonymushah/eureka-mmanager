4505f2d8b8dae654b88b34f1d01637fea4951f3a
tonymushah <tonyramanantsalama@gmail.com>
2024-06-13 00:47:32 +0300

    Added `CoverDataPullAsyncTrait` with implementations

--- src/files_dirs/messages/pull/cover.rs
+++ src/files_dirs/messages/pull/cover.rs
@@ -3,8 +3,54 @@ pub mod cover_ids_list_data_pull;
 pub mod cover_image_data_pull;
 pub mod cover_list_data_pull;
 
+use std::future::Future;
+
+use actix::Addr;
+use bytes::Bytes;
+use mangadex_api_schema_rust::v5::CoverObject;
+use uuid::Uuid;
+
+use crate::{
+    data_pulls::cover::{CoverIdsListDataPull, CoverListDataPull},
+    DirsOptions, MailBoxResult, ManagerCoreResult,
+};
+
 pub use self::{
     cover_data_pull::CoverDataPullMessage, cover_ids_list_data_pull::CoverIdsListDataPullMessage,
     cover_image_data_pull::CoverImageDataPullMessage,
     cover_list_data_pull::CoverListDataPullMessage,
 };
+
+pub trait CoverDataPullAsyncTrait: Sync {
+    fn get_cover(&self, id: Uuid) -> impl Future<Output = ManagerCoreResult<CoverObject>> + Send;
+    fn get_cover_image(&self, id: Uuid) -> impl Future<Output = ManagerCoreResult<Bytes>> + Send;
+    fn get_covers(&self) -> impl Future<Output = ManagerCoreResult<CoverListDataPull>> + Send;
+    fn get_covers_by_ids(
+        &self,
+        ids: impl Iterator<Item = Uuid>,
+    ) -> impl Future<Output = MailBoxResult<CoverIdsListDataPull>> + Send;
+    fn get_cover_with_image(
+        &self,
+        id: Uuid,
+    ) -> impl Future<Output = ManagerCoreResult<(CoverObject, Bytes)>> + Send {
+        async move { Ok((self.get_cover(id).await?, self.get_cover_image(id).await?)) }
+    }
+}
+
+impl CoverDataPullAsyncTrait for Addr<DirsOptions> {
+    async fn get_cover(&self, id: Uuid) -> ManagerCoreResult<CoverObject> {
+        self.send(CoverDataPullMessage(id)).await?
+    }
+    async fn get_cover_image(&self, id: Uuid) -> ManagerCoreResult<Bytes> {
+        self.send(CoverImageDataPullMessage(id)).await?
+    }
+    async fn get_covers(&self) -> ManagerCoreResult<CoverListDataPull> {
+        self.send(CoverListDataPullMessage).await?
+    }
+    fn get_covers_by_ids(
+        &self,
+        ids: impl Iterator<Item = Uuid>,
+    ) -> impl Future<Output = MailBoxResult<CoverIdsListDataPull>> + Send {
+        self.send(CoverIdsListDataPullMessage(ids.collect()))
+    }
+}
