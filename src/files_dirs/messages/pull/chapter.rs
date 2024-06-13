e6570ae1f019c0d652cb42a8588e116ea3867d40
tonymushah <tonyramanantsalama@gmail.com>
2024-06-13 08:40:35 +0300

    Added `ChapterDataPullAsyncTrait` with implementation

--- src/files_dirs/messages/pull/chapter.rs
+++ src/files_dirs/messages/pull/chapter.rs
@@ -5,6 +5,20 @@ pub mod chapter_image_data_pull;
 pub mod chapter_image_data_saver_pull;
 pub mod chapter_list_data_pull;
 
+use std::{future::Future, path::Path};
+
+use actix::Addr;
+use bytes::Bytes;
+use mangadex_api_schema_rust::v5::ChapterObject;
+use uuid::Uuid;
+
+use crate::{
+    data_pulls::chapter::{
+        ids::ChapterIdsListDataPull, images::ChapterImagesData, list::ChapterListDataPull,
+    },
+    DirsOptions, MailBoxResult, ManagerCoreResult,
+};
+
 pub use self::{
     chapter_at_home_pull::ChapterImagesPullMessage, chapter_data_pull::ChapterDataPullMessage,
     chapter_ids_list_data_pull::ChapterIdsListDataPullMessage,
@@ -12,3 +26,62 @@ pub use self::{
     chapter_image_data_saver_pull::ChapterImageDataSaverPullMessage,
     chapter_list_data_pull::ChapterListDataPullMessage,
 };
+
+pub trait ChapterDataPullAsyncTrait: Sync {
+    fn get_chapter(
+        &self,
+        id: Uuid,
+    ) -> impl Future<Output = ManagerCoreResult<ChapterObject>> + Send;
+    fn get_chapter_images(
+        &self,
+        id: Uuid,
+    ) -> impl Future<Output = ManagerCoreResult<ChapterImagesData>> + Send;
+    fn get_chapters(&self) -> impl Future<Output = ManagerCoreResult<ChapterListDataPull>> + Send;
+    fn get_chapter_image(
+        &self,
+        id: Uuid,
+        filename: impl AsRef<Path> + Send + 'static,
+    ) -> impl Future<Output = ManagerCoreResult<Bytes>> + Send;
+    fn get_chapter_image_data_saver(
+        &self,
+        id: Uuid,
+        filename: impl AsRef<Path> + Send + 'static,
+    ) -> impl Future<Output = ManagerCoreResult<Bytes>> + Send;
+    fn get_chapters_by_ids(
+        &self,
+        ids: impl Iterator<Item = Uuid> + Send,
+    ) -> impl Future<Output = MailBoxResult<ChapterIdsListDataPull>> + Send;
+}
+
+impl ChapterDataPullAsyncTrait for Addr<DirsOptions> {
+    async fn get_chapter(&self, id: Uuid) -> ManagerCoreResult<ChapterObject> {
+        self.send(ChapterDataPullMessage(id)).await?
+    }
+    async fn get_chapter_images(&self, id: Uuid) -> ManagerCoreResult<ChapterImagesData> {
+        self.send(ChapterImagesPullMessage(id)).await?
+    }
+    async fn get_chapters(&self) -> ManagerCoreResult<ChapterListDataPull> {
+        self.send(ChapterListDataPullMessage).await?
+    }
+    async fn get_chapter_image(
+        &self,
+        id: Uuid,
+        filename: impl AsRef<Path> + Send + 'static,
+    ) -> ManagerCoreResult<Bytes> {
+        self.send(ChapterImageDataPullMessage(id, filename)).await?
+    }
+    async fn get_chapter_image_data_saver(
+        &self,
+        id: Uuid,
+        filename: impl AsRef<Path> + Send + 'static,
+    ) -> ManagerCoreResult<Bytes> {
+        self.send(ChapterImageDataSaverPullMessage(id, filename))
+            .await?
+    }
+    fn get_chapters_by_ids(
+        &self,
+        ids: impl Iterator<Item = Uuid>,
+    ) -> impl Future<Output = MailBoxResult<ChapterIdsListDataPull>> + Send {
+        self.send(ChapterIdsListDataPullMessage(ids.collect()))
+    }
+}
