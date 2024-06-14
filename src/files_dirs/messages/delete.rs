e3b2db19575eb7fd362fb51350b12a3709e1c7f3
tonymushah <tonyramanantsalama@gmail.com>
2024-06-14 08:41:11 +0300

    implemented `DeleteDataAsyncTrait` for `GetManagerStateData`

--- src/files_dirs/messages/delete.rs
+++ src/files_dirs/messages/delete.rs
@@ -9,7 +9,7 @@ use chapter::images::ChapterImages;
 use manga::MangaDeleteData;
 use uuid::Uuid;
 
-use crate::{DirsOptions, ManagerCoreResult};
+use crate::{download::state::messages::get::GetManagerStateData, DirsOptions, ManagerCoreResult};
 
 pub use self::{
     chapter::{images::DeleteChapterImagesMessage, DeleteChapterMessage},
@@ -60,5 +60,40 @@ impl DeleteDataAsyncTrait for Addr<DirsOptions> {
     }
     async fn delete_manga(&self, id: Uuid) -> ManagerCoreResult<MangaDeleteData> {
         self.send(DeleteMangaMessage(id)).await?
+    }
+}
+
+impl<A> DeleteDataAsyncTrait for A
+where
+    A: GetManagerStateData,
+{
+    async fn delete_chapter_images(
+        &self,
+        id: Uuid,
+        mode: impl Into<ChapterImages> + Send + 'static,
+    ) -> ManagerCoreResult<()> {
+        self.get_dir_options()
+            .await?
+            .delete_chapter_images(id, mode)
+            .await
+    }
+    async fn delete_chapter_images_ignore_conflict(
+        &self,
+        id: Uuid,
+        mode: impl Into<ChapterImages> + Send + 'static,
+    ) -> ManagerCoreResult<()> {
+        self.get_dir_options()
+            .await?
+            .delete_chapter_images_ignore_conflict(id, mode)
+            .await
+    }
+    async fn delete_chapter(&self, id: Uuid) -> ManagerCoreResult<()> {
+        self.get_dir_options().await?.delete_chapter(id).await
+    }
+    async fn delete_cover(&self, id: Uuid) -> ManagerCoreResult<()> {
+        self.get_dir_options().await?.delete_cover(id).await
+    }
+    async fn delete_manga(&self, id: Uuid) -> ManagerCoreResult<MangaDeleteData> {
+        self.get_dir_options().await?.delete_manga(id).await
     }
 }
