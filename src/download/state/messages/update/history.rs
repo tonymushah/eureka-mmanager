2381c893d0729e572ec6e389c73d52bdd1cb96eb
tonymushah <tonyramanantsalama@gmail.com>
2024-06-14 06:25:53 +0300

    updated `UpdateHistoryMessage` response to `Addr<HistoryActorService>`

--- src/download/state/messages/update/history.rs
+++ src/download/state/messages/update/history.rs
@@ -1,17 +1,19 @@
 use actix::prelude::*;
 
+use std::mem;
+
 use crate::{download::state::DownloadManagerState, history::service::HistoryActorService};
 
 #[derive(Debug, Clone)]
 pub struct UpdateHistoryMessage(pub Addr<HistoryActorService>);
 
 impl Message for UpdateHistoryMessage {
-    type Result = ();
+    type Result = Addr<HistoryActorService>;
 }
 
 impl Handler<UpdateHistoryMessage> for DownloadManagerState {
     type Result = <UpdateHistoryMessage as Message>::Result;
     fn handle(&mut self, msg: UpdateHistoryMessage, _ctx: &mut Self::Context) -> Self::Result {
-        self.history = msg.0;
+        mem::replace(&mut self.history, msg.0)
     }
 }
