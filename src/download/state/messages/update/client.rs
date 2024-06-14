05a97309d5afede06200a410fd6ec8b89943f2b2
tonymushah <tonyramanantsalama@gmail.com>
2024-06-14 05:57:47 +0300

    updated `UpdateClientMessage` response to `MangaDexClient`

--- src/download/state/messages/update/client.rs
+++ src/download/state/messages/update/client.rs
@@ -1,18 +1,20 @@
 use actix::prelude::*;
 use mangadex_api::MangaDexClient;
 
+use std::mem;
+
 use crate::download::state::DownloadManagerState;
 
 #[derive(Debug, Clone)]
 pub struct UpdateClientMessage(pub MangaDexClient);
 
 impl Message for UpdateClientMessage {
-    type Result = ();
+    type Result = MangaDexClient;
 }
 
 impl Handler<UpdateClientMessage> for DownloadManagerState {
     type Result = <UpdateClientMessage as Message>::Result;
     fn handle(&mut self, msg: UpdateClientMessage, _ctx: &mut Self::Context) -> Self::Result {
-        self.client = msg.0;
+        mem::replace(&mut self.client, msg.0)
     }
 }
