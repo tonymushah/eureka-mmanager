c98fcd86f595ce6eefd001d33bafc328bdc631c2
tonymushah <tonyramanantsalama@gmail.com>
2024-06-14 06:35:35 +0300

    Added `GetManagerState` with implementations

--- src/download/messages/state.rs
+++ src/download/messages/state.rs
@@ -1,6 +1,12 @@
-use actix::prelude::*;
+use std::future::Future;
 
-use crate::download::{state::DownloadManagerState, DownloadManager};
+use actix::prelude::*;
+use dev::ToEnvelope;
+
+use crate::{
+    download::{state::DownloadManagerState, DownloadManager},
+    MailBoxResult,
+};
 
 #[derive(Debug, Clone, Copy, Default)]
 pub struct GetManagerStateMessage;
@@ -9,16 +15,36 @@ impl Message for GetManagerStateMessage {
     type Result = Addr<DownloadManagerState>;
 }
 
+/*
 impl Handler<GetManagerStateMessage> for DownloadManagerState {
     type Result = <GetManagerStateMessage as Message>::Result;
     fn handle(&mut self, _msg: GetManagerStateMessage, ctx: &mut Self::Context) -> Self::Result {
         ctx.address()
     }
 }
+*/
 
 impl Handler<GetManagerStateMessage> for DownloadManager {
     type Result = <GetManagerStateMessage as Message>::Result;
     fn handle(&mut self, _msg: GetManagerStateMessage, _ctx: &mut Self::Context) -> Self::Result {
         self.state.clone()
+    }
+}
+
+pub trait GetManagerState: Sync {
+    fn get_manager_state(
+        &self,
+    ) -> impl Future<Output = MailBoxResult<Addr<DownloadManagerState>>> + Send;
+}
+
+impl<A> GetManagerState for Addr<A>
+where
+    A: Actor + Handler<GetManagerStateMessage>,
+    <A as Actor>::Context: ToEnvelope<A, GetManagerStateMessage>,
+{
+    fn get_manager_state(
+        &self,
+    ) -> impl Future<Output = MailBoxResult<Addr<DownloadManagerState>>> + Send {
+        self.send(GetManagerStateMessage)
     }
 }
