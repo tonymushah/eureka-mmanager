1afa6daa384ab8941274e40ab0dc3245ac156af4
tonymushah <tonyramanantsalama@gmail.com>
2024-06-18 17:07:37 +0300

    delete_manga_test refactor

--- examples/delete_manga_test.rs
+++ examples/delete_manga_test.rs
@@ -6,14 +6,18 @@ use tokio_stream::StreamExt;
 use uuid::Uuid;
 
 fn main() -> anyhow::Result<()> {
+    // Init the actix system runner
     let run = System::new();
     run.block_on(async {
-        let options = DirsOptions::new_from_data_dir("data");
-        options.verify_and_init()?;
-        let options_actor = options.start();
+        // Start the option actor
+        let options_actor = DirsOptions::new_from_data_dir("data").start();
         let manga_id = Uuid::from_str("b4c93297-b32f-4f90-b619-55456a38b0aa")?;
+        // You can just call `.delete_manga(Uuid)` to delete a give manga
         let data = options_actor.delete_manga(manga_id).await?;
+        // The `MangaDeleteData` consists of `covers` field which is the deleted covers ids
+        // and `chapters` field which is the deleted chapters ids
         println!("{:#?}", data);
+        // Get all the manga chapter
         let chapters: Vec<Uuid> = {
             let params = ChapterListDataPullFilterParams {
                 manga_id: Some(manga_id),
@@ -40,8 +44,10 @@ fn main() -> anyhow::Result<()> {
                 .collect()
                 .await
         };
-        println!("{:#?}", chapters);
-        println!("{:#?}", covers);
+        // check if there is no chapters left
+        assert!(chapters.is_empty(), "Some chapter still remains");
+        // check if there is no covers left
+        assert!(covers.is_empty(), "Some covers still remains");
         Ok::<(), anyhow::Error>(())
     })?;
     Ok(())
