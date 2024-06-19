87c48aa70bf3c1bbe062d100a4997b8e419db9f6
tonymushah <tonyramanantsalama@gmail.com>
2024-06-19 11:51:28 +0300

    Added docs to `AsyncIntoSorted` and `IntoSorted`

--- src/data_pulls/sort.rs
+++ src/data_pulls/sort.rs
@@ -1,3 +1,8 @@
+/// This allows you sort your [`tokio_stream::Stream`] with some sort of parameter [`P`].
+///
+/// Please note that it returns a [`Vec`].
+/// Which means that you need to collect all the stream data and then implement sort code after that.
+/// It's recommended to implement [`IntoSorted`] first to avoid having a long code.
 pub trait AsyncIntoSorted<P> {
     type Item;
     fn to_sorted(
@@ -6,6 +11,11 @@ pub trait AsyncIntoSorted<P> {
     ) -> impl std::future::Future<Output = Vec<<Self as AsyncIntoSorted<P>>::Item>> + Send;
 }
 
+/// This allows you sort your [`Vec`] or [`Iterator`] (which I tried but It doesn't work) with some sort of parameter [`P`].
+///
+/// __But what's the difference between [`[T]::sort_by`](https://doc.rust-lang.org/std/primitive.slice.html#method.sort_by)?__
+///
+/// Well, if you need to sort something more often with a set of parameters then this is the perfect solution for you.
 pub trait IntoSorted<P> {
     type Item;
     fn to_sorted(self, params: P) -> Vec<<Self as IntoSorted<P>>::Item>;
