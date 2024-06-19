3d82451e7b9094a43883757afc5e9d7b4e31eb34
tonymushah <tonyramanantsalama@gmail.com>
2024-06-19 08:23:56 +0300

    Added `with_available_langs` for `Handler<MangaListDataPullMessage> for DirsOptions`

--- src/files_dirs/messages/pull/manga/manga_list_data_pull.rs
+++ src/files_dirs/messages/pull/manga/manga_list_data_pull.rs
@@ -1,6 +1,15 @@
-use actix::prelude::*;
+use std::collections::HashMap;
 
-use crate::{data_pulls::manga::list::MangaListDataPull, DirsOptions, ManagerCoreResult};
+use actix::prelude::*;
+use mangadex_api_types_rust::{Language, RelationshipType};
+use tokio_stream::StreamExt;
+use uuid::Uuid;
+
+use crate::{
+    data_pulls::manga::list::MangaListDataPull,
+    files_dirs::messages::pull::chapter::ChapterListDataPullMessage, DirsOptions,
+    ManagerCoreResult,
+};
 
 #[derive(Debug, Clone, Copy, Hash, Default)]
 pub struct MangaListDataPullMessage;
@@ -10,8 +19,30 @@ impl Message for MangaListDataPullMessage {
 }
 
 impl Handler<MangaListDataPullMessage> for DirsOptions {
-    type Result = ManagerCoreResult<MangaListDataPull>;
-    fn handle(&mut self, _msg: MangaListDataPullMessage, _ctx: &mut Self::Context) -> Self::Result {
-        MangaListDataPull::new(self.mangas.clone())
+    type Result = ResponseFuture<ManagerCoreResult<MangaListDataPull>>;
+    fn handle(&mut self, _msg: MangaListDataPullMessage, ctx: &mut Self::Context) -> Self::Result {
+        let pull = MangaListDataPull::new(self.mangas.clone());
+        let chap_pull = self.handle(ChapterListDataPullMessage, ctx).map(|p| {
+            p.fold(HashMap::<Uuid, Vec<Language>>::new(), |mut acc, value| {
+                if let Some(manga_id) = value
+                    .find_first_relationships(RelationshipType::Manga)
+                    .map(|rel| rel.id)
+                {
+                    let current_lang = value.attributes.translated_language;
+                    let langs = acc.entry(manga_id).or_default();
+                    if !langs.contains(&current_lang) {
+                        langs.push(current_lang);
+                    }
+                }
+                acc
+            })
+        });
+        Box::pin(async move {
+            let mut pull = pull?;
+            if let Ok(langs_fut) = chap_pull {
+                pull = pull.with_available_langs(langs_fut.await);
+            }
+            Ok(pull)
+        })
     }
 }
