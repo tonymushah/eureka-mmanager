31d17f91e7e0c04ed0ebdd5bd7db276be2c98a03
tonymushah <tonyramanantsalama@gmail.com>
2024-06-19 10:40:34 +0300

    Added alter `available_translated_languages` for `MangaDataPullMessage`

--- src/files_dirs/messages/pull/manga/manga_data_pull.rs
+++ src/files_dirs/messages/pull/manga/manga_data_pull.rs
@@ -1,8 +1,15 @@
 use actix::prelude::*;
 use mangadex_api_schema_rust::v5::MangaObject;
+use mangadex_api_types_rust::Language;
+use tokio_stream::StreamExt;
 use uuid::Uuid;
 
-use crate::{data_pulls::Pull, DirsOptions, ManagerCoreResult};
+use crate::{
+    data_pulls::Pull,
+    files_dirs::messages::pull::chapter::ChapterListDataPullMessage,
+    prelude::{ChapterListDataPullFilterParams, IntoParamedFilteredStream},
+    DirsOptions, ManagerCoreResult,
+};
 
 #[derive(Debug, Clone, Hash, Default)]
 pub struct MangaDataPullMessage(pub Uuid);
@@ -24,9 +31,30 @@ impl Message for MangaDataPullMessage {
 }
 
 impl Handler<MangaDataPullMessage> for DirsOptions {
-    type Result = <MangaDataPullMessage as Message>::Result;
+    type Result = ResponseFuture<<MangaDataPullMessage as Message>::Result>;
 
-    fn handle(&mut self, msg: MangaDataPullMessage, _ctx: &mut Self::Context) -> Self::Result {
-        self.pull(msg.into())
+    fn handle(&mut self, msg: MangaDataPullMessage, ctx: &mut Self::Context) -> Self::Result {
+        let manga: ManagerCoreResult<MangaObject> = self.pull(msg.into());
+        let chapter_pull = self.handle(ChapterListDataPullMessage, ctx);
+        Box::pin(async move {
+            let mut manga = manga?;
+            if let Ok(pull) = chapter_pull {
+                let langs = pull
+                    .to_filtered(ChapterListDataPullFilterParams {
+                        manga_id: Some(manga.id),
+                        ..Default::default()
+                    })
+                    .fold(Vec::<Language>::new(), |mut acc, chapter| {
+                        let lang = chapter.attributes.translated_language;
+                        if !acc.contains(&lang) {
+                            acc.push(lang);
+                        }
+                        acc
+                    })
+                    .await;
+                manga.attributes.available_translated_languages = langs;
+            }
+            Ok(manga)
+        })
     }
 }
