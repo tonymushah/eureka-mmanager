09d2de002ac309cfba20c447aafa95f929578b5b
tonymushah <tonyramanantsalama@gmail.com>
2024-07-24 14:24:09 +0300

    Added `time` bench in `package.rs`

--- crates/edmx/src/bin/package.rs
+++ crates/edmx/src/bin/package.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::BufWriter};
+use std::{fs::File, io::BufWriter, time::Instant};
 
 use api_core::{
     data_pulls::{
@@ -11,6 +11,7 @@ use edmx::PackageBuilder;
 use mangadex_api_types_rust::Language;
 
 fn main() {
+    let start = Instant::now();
     let options = DirsOptions::new_from_data_dir("data");
     let mut builder = PackageBuilder::new(options.clone());
     let chapters = {
@@ -37,10 +38,18 @@ fn main() {
             .map(|chap| chap.id)
             .collect::<Vec<_>>()
     };
+    let pull_time = Instant::now() - start;
+    let start = Instant::now();
     for chapter_id in chapters {
         builder.add_chapter(chapter_id, Default::default()).unwrap();
     }
+    let add_time = Instant::now() - start;
+    let start = Instant::now();
     let mut output_file = File::create("target/fuufu-ijou-v8-v9-en.tar.zstd").unwrap();
-    builder.build(BufWriter::new(&mut output_file)).unwrap();
+    let _ = builder.build(BufWriter::new(&mut output_file)).unwrap();
+    let build_time = Instant::now() - start;
     println!("Done!");
+    println!("Pulling Time: {} ms", pull_time.as_millis());
+    println!("Adding Time: {} ms", add_time.as_millis());
+    println!("Build Time: {} ms", build_time.as_millis());
 }
