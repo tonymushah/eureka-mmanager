8541f81129c270369c23ec357164453ff329f45a
tonymushah <tonyramanantsalama@gmail.com>
2024-07-24 17:19:48 +0300

    some refactor in `package.rs`

--- crates/edmx/src/bin/package.rs
+++ crates/edmx/src/bin/package.rs
@@ -1,6 +1,7 @@
 use std::{
     fs::File,
     io::{BufWriter, Write},
+    thread,
     time::Instant,
 };
 
@@ -13,6 +14,74 @@ use api_core::{
 };
 use edmx::PackageBuilder;
 use mangadex_api_types_rust::Language;
+
+fn normal(builder: PackageBuilder) {
+    let start = Instant::now();
+    let mut output_file = File::create("target/fuufu-ijou-v8-v9-en.tar.zstd").unwrap();
+    let mut output_file_buf_writer = BufWriter::new(&mut output_file);
+    let _ = builder.clone().build(&mut output_file_buf_writer).unwrap();
+    output_file_buf_writer.flush().unwrap();
+    let build_time = Instant::now() - start;
+    println!("Build Time Normal: {} s", build_time.as_secs_f64());
+}
+fn zstd_images(builder: PackageBuilder) {
+    let start = Instant::now();
+    let mut output_file = File::create("target/fuufu-ijou-v8-v9-en-zstd-images.tar.zstd").unwrap();
+    let mut output_file_buf_writer = BufWriter::new(&mut output_file);
+    let _ = {
+        let mut b = builder.clone();
+        b.zstd_compressed_images(true);
+        b
+    }
+    .build(&mut output_file_buf_writer)
+    .unwrap();
+    output_file_buf_writer.flush().unwrap();
+    let build_time = Instant::now() - start;
+    println!(
+        "Build Time Zstd compressed images: {} s",
+        build_time.as_secs_f64()
+    );
+}
+
+fn zstd_metadata(builder: PackageBuilder) {
+    let start = Instant::now();
+    let mut output_file =
+        File::create("target/fuufu-ijou-v8-v9-en-zstd-metadata.tar.zstd").unwrap();
+    let mut output_file_buf_writer = BufWriter::new(&mut output_file);
+    let _ = {
+        let mut b = builder.clone();
+        b.zstd_compressed_images(true);
+        b
+    }
+    .build(&mut output_file_buf_writer)
+    .unwrap();
+    output_file_buf_writer.flush().unwrap();
+    let build_time = Instant::now() - start;
+    println!(
+        "Build Time Zstd compressed metadata: {} s",
+        build_time.as_secs_f64()
+    );
+}
+
+fn zstd_all(builder: PackageBuilder) {
+    let start = Instant::now();
+    let mut output_file = File::create("target/fuufu-ijou-v8-v9-en-zstd-all.tar.zstd").unwrap();
+    let mut output_file_buf_writer = BufWriter::new(&mut output_file);
+    let _ = {
+        let mut b = builder.clone();
+        b.zstd_compressed_images(true);
+        b.zstd_compressed_metadata(true);
+        b
+    }
+    .build(&mut output_file_buf_writer)
+    .unwrap();
+    output_file_buf_writer.flush().unwrap();
+    let build_time = Instant::now() - start;
+    println!(
+        "Build Time Zstd compressed all: {} s",
+        build_time.as_secs_f64()
+    );
+}
 
 fn main() {
     let start = Instant::now();
@@ -48,14 +117,20 @@ fn main() {
     for chapter_id in chapters {
         builder.add_chapter(chapter_id, Default::default()).unwrap();
     }
+    builder.set_compression_level(22);
     let add_time = Instant::now() - start;
     println!("Adding Time: {} ms", add_time.as_millis());
-    let start = Instant::now();
-    let mut output_file = File::create("target/fuufu-ijou-v8-v9-en.tar.zstd").unwrap();
-    let mut output_file_buf_writer = BufWriter::new(&mut output_file);
-    let _ = builder.build(&mut output_file_buf_writer).unwrap();
-    output_file_buf_writer.flush().unwrap();
-    let build_time = Instant::now() - start;
+    let bn = builder.clone();
+    let bzi = builder.clone();
+    let bzm = builder.clone();
+    let bza = builder.clone();
+    let n = thread::spawn(move || normal(bn));
+    let zi = thread::spawn(move || zstd_images(bzi));
+    let zm = thread::spawn(move || zstd_metadata(bzm));
+    let za = thread::spawn(move || zstd_all(bza));
+    n.join().unwrap();
+    zi.join().unwrap();
+    zm.join().unwrap();
+    za.join().unwrap();
     println!("Done!");
-    println!("Build Time: {} s", build_time.as_secs_f64());
 }
