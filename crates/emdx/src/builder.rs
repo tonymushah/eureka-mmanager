1a5f7e655ba26df29656e96d131b39b3256c7ece
tonymushah <tonyramanantsalama@gmail.com>
2024-08-23 13:12:08 +0300

    added `header_mode` in `Builder`

--- crates/emdx/src/builder.rs
+++ crates/emdx/src/builder.rs
@@ -18,16 +18,30 @@ use inner::BuilderInner;
 use mangadex_api_schema_rust::v5::{ChapterObject, CoverObject, MangaObject};
 use mangadex_api_types_rust::RelationshipType;
 
+use tar::HeaderMode;
 use uuid::Uuid;
 
 use crate::{PMangaObject, PackageContents, ThisResult};
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct Builder {
     initial_dir_options: DirsOptions,
     contents: PackageContents,
     compression_level: i32,
     compress_image_to_jpeg: bool,
+    header_mode: HeaderMode,
+}
+
+impl Default for Builder {
+    fn default() -> Self {
+        Self {
+            initial_dir_options: Default::default(),
+            contents: Default::default(),
+            compression_level: Default::default(),
+            compress_image_to_jpeg: Default::default(),
+            header_mode: HeaderMode::Complete,
+        }
+    }
 }
 
 impl TryFrom<DirsOptions> for Builder {
@@ -76,6 +90,10 @@ impl Builder {
     }
     pub fn set_compress_image_to_jpeg(mut self, compress_image_to_jpeg: bool) -> Self {
         self.compress_image_to_jpeg = compress_image_to_jpeg;
+        self
+    }
+    pub fn set_tar_header_mode(mut self, mode: HeaderMode) -> Self {
+        self.header_mode = mode;
         self
     }
     pub fn set_content(mut self, content: PackageContents) -> Self {
