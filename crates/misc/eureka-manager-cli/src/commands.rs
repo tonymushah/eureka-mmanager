cdb533585ac3c8fd4a27816b09e9676aca36083e
tonymushah <tonyramanantsalama@gmail.com>
2024-12-04 13:04:53 +0300

    DownloadSubCommands base

--- crates/misc/eureka-manager-cli/src/commands.rs
+++ crates/misc/eureka-manager-cli/src/commands.rs
@@ -1,4 +1,11 @@
+pub mod download;
+
 use clap::Subcommand;
+use download::DownloadSubCommands;
 
 #[derive(Debug, Subcommand)]
-pub enum Commands {}
+pub enum Commands {
+    /// Download subcommands
+    #[command(subcommand)]
+    Download(DownloadSubCommands),
+}
