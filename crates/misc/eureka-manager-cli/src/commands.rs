600700388bff2c8a8d4506d269bbc8fcc4b8498a
tonymushah <tonyramanantsalama@gmail.com>
2024-12-09 23:51:42 +0300

    Added `AsyncRunable` trait and implemented for `MangaDownloadArgs`

--- crates/misc/eureka-manager-cli/src/commands.rs
+++ crates/misc/eureka-manager-cli/src/commands.rs
@@ -1,11 +1,22 @@
 pub mod download;
 
+use std::future::Future;
+
+use actix::Addr;
 use clap::Subcommand;
 use download::DownloadSubCommands;
+use eureka_mmanager::DownloadManager;
 
 #[derive(Debug, Subcommand)]
 pub enum Commands {
     /// Download subcommands
     #[command(subcommand)]
     Download(DownloadSubCommands),
+}
+
+pub trait AsyncRun: Sync {
+    fn run(
+        &self,
+        manager: Addr<DownloadManager>,
+    ) -> impl Future<Output = anyhow::Result<()>> + Send;
 }
--- crates/misc/eureka-manager-cli/src/commands/download/manga.rs
+++ crates/misc/eureka-manager-cli/src/commands/download/manga.rs
@@ -1,16 +1,26 @@
 use std::{fs::File, io::BufReader, path::PathBuf, str::FromStr};
 
+use actix::Addr;
 use clap::Args;
+use eureka_mmanager::{
+    download::{
+        cover::CoverDownloadMessage, manga::MangaDownloadMessage, state::DownloadMessageState,
+    },
+    history::service::messages::is_in::IsInMessage,
+    prelude::*,
+};
+use log::{info, trace};
+use mangadex_api_types_rust::RelationshipType;
+use tokio::task::JoinSet;
 use uuid::Uuid;
+
+use crate::commands::AsyncRun;
 
 #[derive(Debug, Args)]
 pub struct MangaDownloadArgs {
     /// Manga ids
     #[arg(long = "id")]
     pub ids: Vec<Uuid>,
-    /// number
-    #[arg(long, default_value_t = 5)]
-    pub barrier: u16,
     #[arg(long)]
     pub id_text_file: Vec<PathBuf>,
 }
@@ -34,6 +44,71 @@ impl MangaDownloadArgs {
             .for_each(|id| {
                 ids.push(id);
             });
+        ids.dedup();
         ids
+    }
+}
+
+impl AsyncRun for MangaDownloadArgs {
+    async fn run(&self, manager: Addr<DownloadManager>) -> anyhow::Result<()> {
+        let mut tasks = JoinSet::<anyhow::Result<()>>::new();
+        let ids = self.get_ids();
+        trace!("Downloading {} titles with their cover", ids.len());
+        for id in ids {
+            let manager = manager.clone();
+            tasks.spawn(async move {
+                trace!("Downloading title {id}");
+                let dirs =
+                    <Addr<DownloadManager> as GetManagerStateData>::get_dir_options(&manager)
+                        .await?;
+                let cover = {
+                    let manga_manager =
+                        <Addr<DownloadManager> as GetManager<MangaDownloadManager>>::get(&manager)
+                            .await?;
+                    let mut task = manga_manager
+                        .send(
+                            MangaDownloadMessage::new(id).state(DownloadMessageState::Downloading),
+                        )
+                        .await?;
+                    let data = task.wait().await?.await?;
+                    info!(
+                        "downloaded title {} = {:?}",
+                        data.id,
+                        data.attributes.title.values().next()
+                    );
+                    data.find_first_relationships(RelationshipType::CoverArt)
+                        .ok_or(anyhow::Error::msg(format!(
+                            "Cannot find the title {} cover art",
+                            id
+                        )))?
+                        .clone()
+                };
+                if !dirs
+                    .send(IsInMessage(HistoryEntry::new(
+                        cover.id,
+                        RelationshipType::CoverArt,
+                    )))
+                    .await?
+                {
+                    trace!("Downloading {} cover art", cover.id);
+                    let cover_manager =
+                        <Addr<DownloadManager> as GetManager<CoverDownloadManager>>::get(&manager)
+                            .await?;
+                    let mut task = cover_manager
+                        .send(
+                            CoverDownloadMessage::new(cover.id)
+                                .state(DownloadMessageState::Downloading),
+                        )
+                        .await?;
+                    task.wait().await?.await?;
+                    info!("Downloaded {} cover art", cover.id);
+                }
+                Ok(())
+            });
+        }
+        while let Some(task) = tasks.join_next().await {
+            task??;
+        }
+        Ok(())
     }
 }
