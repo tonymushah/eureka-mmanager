87ca58cc5213a754d4473886ad479e58ef990751
tonymushah <tonyramanantsalama@gmail.com>
2024-12-10 20:36:32 +0300

    fixed `transfer` `ids` debug assert

--- crates/misc/eureka-manager-cli/src/commands/transfer.rs
+++ crates/misc/eureka-manager-cli/src/commands/transfer.rs
@@ -22,16 +22,16 @@ pub struct TransferCommand {
 
 #[derive(Debug, Args)]
 pub struct TransferMangaArgs {
-    #[arg(long = "manga")]
-    pub ids: Vec<Uuid>,
-    #[arg(long = "manga-id-text-file")]
-    pub id_text_file: Vec<PathBuf>,
+    #[arg(long)]
+    pub manga: Vec<Uuid>,
+    #[arg(long)]
+    pub manga_ids_text_file: Vec<PathBuf>,
 }
 
 impl TransferMangaArgs {
     pub fn get_ids(&self) -> Vec<Uuid> {
-        let mut ids = self.ids.clone();
-        self.id_text_file
+        let mut ids = self.manga.clone();
+        self.manga_ids_text_file
             .iter()
             .map(|e| (e, File::open(e)))
             .flat_map(|(path, res)| match res {
@@ -54,16 +54,16 @@ impl TransferMangaArgs {
 
 #[derive(Debug, Args)]
 pub struct TransferCoverArgs {
-    #[arg(long = "cover")]
-    pub ids: Vec<Uuid>,
-    #[arg(long = "cover-id-text-file")]
-    pub id_text_file: Vec<PathBuf>,
+    #[arg(long)]
+    pub cover: Vec<Uuid>,
+    #[arg(long)]
+    pub cover_ids_text_file: Vec<PathBuf>,
 }
 
 impl TransferCoverArgs {
     pub fn get_ids(&self) -> Vec<Uuid> {
-        let mut ids = self.ids.clone();
-        self.id_text_file
+        let mut ids = self.cover.clone();
+        self.cover_ids_text_file
             .iter()
             .map(|e| (e, File::open(e)))
             .flat_map(|(path, res)| match res {
@@ -86,16 +86,16 @@ impl TransferCoverArgs {
 
 #[derive(Debug, Args)]
 pub struct TransferChapterArgs {
-    #[arg(long = "chapter")]
-    pub ids: Vec<Uuid>,
-    #[arg(long = "chapter-id-text-file")]
-    pub id_text_file: Vec<PathBuf>,
+    #[arg(long)]
+    pub chapter: Vec<Uuid>,
+    #[arg(long)]
+    pub chapter_ids_text_file: Vec<PathBuf>,
 }
 
 impl TransferChapterArgs {
     pub fn get_ids(&self) -> Vec<Uuid> {
-        let mut ids = self.ids.clone();
-        self.id_text_file
+        let mut ids = self.chapter.clone();
+        self.chapter_ids_text_file
             .iter()
             .map(|e| (e, File::open(e)))
             .flat_map(|(path, res)| match res {
