5387c128cb18ce60a79d933cbb38df2cf04e71d4
tonymushah <tonyramanantsalama@gmail.com>
2024-12-10 20:01:57 +0300

    Added `chapter` delete action

--- crates/misc/eureka-manager-cli/src/commands/delete.rs
+++ crates/misc/eureka-manager-cli/src/commands/delete.rs
@@ -1,3 +1,4 @@
+pub mod chapter;
 pub mod cover;
 pub mod manga;
 
@@ -7,8 +8,12 @@ use super::AsyncRun;
 
 #[derive(Debug, Subcommand)]
 pub enum DeleteSubcommands {
+    /// Delete Manga subcommand
     Manga(manga::MangaDeleteArgs),
+    /// Delete Cover subcommand
     Cover(cover::CoverDeleteArgs),
+    /// Delete Chapter subcommand
+    Chapter(chapter::ChapterDeleteArgs),
 }
 
 impl AsyncRun for DeleteSubcommands {
@@ -19,6 +24,9 @@ impl AsyncRun for DeleteSubcommands {
         match self {
             DeleteSubcommands::Manga(manga_delete_args) => manga_delete_args.run(manager).await,
             DeleteSubcommands::Cover(cover_delete_args) => cover_delete_args.run(manager).await,
+            DeleteSubcommands::Chapter(chapter_delete_args) => {
+                chapter_delete_args.run(manager).await
+            }
         }
     }
 }
