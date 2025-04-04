af1311565d1092bf77f40941d063f2e405d54908
tonymushah <tonyramanantsalama@gmail.com>
2025-04-04 23:41:46 +0300

    added stopping for `MangaDownloadTask`

--- crates/manager/src/download/manga/task.rs
+++ crates/manager/src/download/manga/task.rs
@@ -1,6 +1,6 @@
 pub mod messages;
 
-use std::{ops::Deref, time::Duration};
+use std::ops::Deref;
 
 use actix::prelude::*;
 use mangadex_api_schema_rust::v5::MangaObject;
@@ -29,20 +29,28 @@ pub struct MangaDownloadTask {
     sender: Sender<MangaDownloadTaskState>,
     manager: Addr<MangaDownloadManager>,
     have_been_read: bool,
-    manager_handle: Option<SpawnHandle>,
 }
 
 impl Actor for MangaDownloadTask {
     type Context = Context<Self>;
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.manager_handle = Some(ctx.run_interval(Duration::from_millis(500), |this, _ctx| {
-            if this.have_been_read
-                && this.sender.is_closed()
-                && std::convert::Into::<TaskState>::into(this.sender.borrow().deref()).is_finished()
-            {
-                this.manager.do_send(DropSingleTaskMessage(this.id));
-            }
-        }));
+    fn stopping(&mut self, ctx: &mut Self::Context) -> Running {
+        if self.have_been_read
+            && self.sender.is_closed()
+            && std::convert::Into::<TaskState>::into(self.sender.borrow().deref()).is_finished()
+        {
+            self.manager
+                .send(DropSingleTaskMessage(self.id))
+                .into_actor(self)
+                .map(|res, _, _| {
+                    if let Err(er) = res {
+                        log::error!("{er}");
+                    }
+                })
+                .wait(ctx);
+            Running::Stop
+        } else {
+            Running::Continue
+        }
     }
 }
 
@@ -55,7 +63,6 @@ impl MangaDownloadTask {
             sender,
             manager,
             have_been_read: false,
-            manager_handle: None,
         }
     }
 }
